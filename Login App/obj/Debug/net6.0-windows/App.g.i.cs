90449d41f9f1b39524ca9deb18070a85d533d78d
Mishanana20 <mishanana20@gmail.com>
2022-10-19 16:59:56 +0700

    Скопировал репозиторий авторизации

--- Login App/obj/Debug/net6.0-windows/App.g.i.cs
+++ Login App/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Login_App {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace Login_App {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Login App;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Login App;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace Login_App {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.10.0")]
         public static void Main() {
             Login_App.App app = new Login_App.App();
             app.InitializeComponent();
